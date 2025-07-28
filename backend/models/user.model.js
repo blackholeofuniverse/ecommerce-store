47c3f8128205275a1d4fb52d6ff504ef98a65740
blackholeofuniverse <omb4505@gmail.com>
2025-07-28 22:15:53 +0530

    user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -37,5 +38,21 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true })
 
 const User = mongoose.model("User", userSchema)
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
 
 export default User
